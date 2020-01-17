6a4ea15790f46fe1a205bd94aa211a6475baec80
Robert M Ricci <rmricci121@gmail.com>
2020-01-17 18:01:10 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Cat
-  # code goes here
+  
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@cats = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner  = owner
+    @mood = "nervous"
+    @@cats << self 
+  end
+  
+  def self.all 
+    @@cats
+  end
+  
+ 
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
+require 'pry'
 class Owner
-  # code goes here
+   
+  attr_reader :name, :species
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+   "I am a #{@species}."
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats 
+    Cat.all.select do |cats|
+      cats.owner == self
+    end
+  end
+  
+  def dogs 
+    Dog.all.select do |dogs|
+      dogs.owner == self
+    end
+  end
+  
 end
