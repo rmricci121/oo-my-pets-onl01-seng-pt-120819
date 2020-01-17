029ba7ef93d5312c9f9606b49cbbbb4398519147
Robert M Ricci <rmricci121@gmail.com>
2020-01-17 19:10:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -71,9 +71,7 @@ class Owner
   end
   
   def list_pets
-   
-       "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
-      #binding.pry
+   "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
    
   
